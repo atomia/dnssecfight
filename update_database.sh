e41de743e52b5df3e7af826e094287e0678fe293
Jimmy Bergman <jimmy@atomia.com>
2012-08-23 16:25:52 +0200

    Fix so that it only updates db when all tlds are in pending

--- update_database.sh
+++ update_database.sh
@@ -14,11 +14,11 @@ ls pending | cut -d . -f 2 | sort -u | grep -E '^[0-9]{8}$' | while read date; d
 	pending_count=`ls pending/zones_with_ds."$date".*.txt | wc -l | tr -d " "`
 	if [ x"$tld_count" = x"$pending_count" ]; then
 		mv pending/zones_with_ds."$date".*.txt .
+
+		./process_zone.sh zones_with_ds."$date".*.txt | grep '^[a-zA-Z0-9.-]*[[:space:]]*[0-9]*$' | \
+			sed 's/^\([a-zA-Z0-9.-]*\)[[:space:]]*\([0-9]*\)$/REPLACE INTO secure_delegation (hoster, day, num) VALUES (#\1#, #'"$date"'#, \2);/' | \
+			tr "#" "'" | $mysql
+
+		mv zones_with_ds."$date".*.txt archived
 	fi
-
-	./process_zone.sh zones_with_ds."$date".*.txt | grep '^[a-zA-Z0-9.-]*[[:space:]]*[0-9]*$' | \
-		sed 's/^\([a-zA-Z0-9.-]*\)[[:space:]]*\([0-9]*\)$/REPLACE INTO secure_delegation (hoster, day, num) VALUES (#\1#, #'"$date"'#, \2);/' | \
-		tr "#" "'" | $mysql
-
-	mv zones_with_ds."$date".*.txt archived
 done
