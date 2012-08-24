1f587f0d4ed0ec2737e7e944dc8caf0c17b7b6ad
Jimmy Bergman <jimmy@atomia.com>
2012-08-24 12:08:25 +0200

    Add fork me on github banner

--- public_html/index.php
+++ public_html/index.php
@@ -38,6 +38,8 @@
 	<body>
 		<h2>DNSSEC operator fight</h2>
 
+		<a href="https://github.com/atomia/dnssecfight"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a>
+
 		<div id="graph" style="width:90%;height:700px;"></div>
 
 		<script type="text/javascript">
